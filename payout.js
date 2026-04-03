ac23dfac58f0cb43a219e3be60a86c3c6df668cd
wiradjuri <bmuzza1992@gmail.com>
2026-04-03 15:56:42 +1100

    Delete payout.js

--- payout.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-	const { amount, reciever } = req.body;
-
-	const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
-
-	res.status(200).json({ success: true, message: "Payout triggered!" });
-}
